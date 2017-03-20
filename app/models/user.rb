7199e390ab32c9f8997e6045d6e27ded7f868e99
ymgn <b4183@oic.jp>
2017-03-20 16:48:19 +0900

    update : パスワードを使えるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :user_skills
   has_many :skill, through: :user_skills
   has_many :reactions
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
