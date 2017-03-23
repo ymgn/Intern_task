19b28636c4fe970bd095b3d1155ef4ae78b43d6c
ymgn <b4183@oic.jp>
2017-03-23 14:30:01 +0900

    add : プロフィール画面からスキルを追加できるように

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  post '/users/:user_id/add_skill', to: 'users#add_skill'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
