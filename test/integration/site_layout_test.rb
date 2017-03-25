8f00e66d85d472d6101421de2f33953175f45f0f
ymgn <b4183@oic.jp>
2017-03-26 00:41:34 +0900

    add layoutのテストを追加

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]","/users"
+    assert_select "a[href=?]",login_path
+  end
+end
